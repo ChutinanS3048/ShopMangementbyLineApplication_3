19da87130bc392459336a9d93266ae0295b37d88
Chutinan Sukata <60294922+ChutinanS3048@users.noreply.github.com>
2022-05-23 15:49:17 +0700

    Create test.php

---
+++ line-bot/test.php
@@ -0,0 +1,24 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+include '../resources/config/line.php';
+
+$userId = '';
+
+$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
