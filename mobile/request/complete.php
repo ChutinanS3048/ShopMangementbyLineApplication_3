f1c735c089e1137dedbb7c03603df05f5ff26799
Chutinan Sukata <60294922+ChutinanS3048@users.noreply.github.com>
2022-05-23 15:23:34 +0700

    Create complete.php

---
+++ mobile/request/complete.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>เลิกสินค้า</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="../resource/jquerymobile/jquery.mobile-1.4.5.min.css" />
+    <script src="../resource/jquerymobile/jquery-1.12.4.js"></script>
+    <script src="../resource/jquerymobile/jquery.mobile-1.4.5.min.js"></script>
+
+    <script language="javascript" type="text/javascript">
+        function windowClose() {
+            window.open('', '_parent', '');
+            window.close();
+        }
+    </script>
+
+</head>
+
+<body>
+
+    <div data-role="page">
+
+        <div data-role="header" data-position="fixed" class="ui-bar">
+            <h1>เบิกสินค้า</h1>
+        </div>
+
+        <div role="main" class="ui-content">
+            <p>
+
+            <div style="text-align: center; display: block; width: 100%;">
+                <h1>ทำรายการเบิกสินค้าเรียบร้อยแล้ว</h1>
+            </div>
+            <p style="text-align: center;">
+                <a href="https://line.me/R/nv/chat" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-icon-check ui-btn-icon-right" data-theme="b">ปิด</a>
+            </p>
+
+            </p>
+        </div>
+
+        <div data-role="footer" data-position="fixed" class="ui-bar" style="bottom:0; position:absolute !important; top: auto !important; width:100%;">
+            <h4>Shop Management</h4>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
