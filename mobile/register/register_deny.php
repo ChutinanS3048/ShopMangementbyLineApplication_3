2441bcc6a4d9e940d9c3f25658984456d31dca70
Chutinan Sukata <60294922+ChutinanS3048@users.noreply.github.com>
2022-05-23 15:22:49 +0700

    Create register_deny.php

---
+++ mobile/register/register_deny.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ลงทะเบียนผู้ใช้ใหม่</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="../resource/jquerymobile/jquery.mobile-1.4.5.min.css" />
+    <script src="../resource/jquerymobile/jquery-1.12.4.js"></script>
+    <script src="../resource/jquerymobile/jquery.mobile-1.4.5.min.js"></script>
+
+</head>
+
+<body>
+    <div data-role="page">
+        <div data-role="header" data-position="fixed" class="ui-bar">
+            <h1>ลงทะเบียนผู้ใช้ใหม่</h1>
+        </div>
+
+        <div role="main" class="ui-content">
+            <p>
+            <div style="text-align: center; display: block; width: 100%;">
+                <h1>คุณเคยลงทะเบียนแล้ว</h1>
+            </div>
+            </p>
+
+            <p style="text-align: center;">
+                <a href="https://line.me/R/nv/chat" class="ui-btn ui-btn-inline ui-shadow ui-corner-all ui-icon-check ui-btn-icon-right" data-theme="b">ปิด</a>
+            </p>
+        </div>
+
+        <div data-role="footer" data-position="fixed" class="ui-bar" style="bottom:0; position:absolute !important; top: auto !important; width:100%;">
+            <h4>Shop Management</h4>
+        </div>
+    </div>
+
+</body>
+
+</html>
